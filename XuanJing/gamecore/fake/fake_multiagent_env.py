eadeb12acae7ac36a5b8e8e844468725c3cd41db
hzq <tinyzqh@163.com>
2022-11-20 12:17:12 +0800

    [dev] update framework

---
+++ XuanJing/gamecore/fake/fake_multiagent_env.py
@@ -0,0 +1,14 @@
+"""
+Fake Gym Environment For Debug.
+"""
+
+
+class MultiAgentFakeCustomEnv(object):
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self):
+        pass
