5b5ef9145208fab6dc33dc7bc0126b88af1435ec
hzq <tinyzqh@163.com>
2023-02-10 00:22:02 +0800

    [dev] modify fictitious

--- XuanJing/algorithms/multiagent/fictitious_play.py
+++ XuanJing/algorithms/multiagent/fictitious_play.py
@@ -1,83 +1,40 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2022/9/27 8:41 下午
-# @Author  : Zhiqiang He
-# @Email   : tinyzqh@163.com
-# @File    : fictitious_play.py
-# @Software: PyCharm
-
 """
 Fictitious Play是找出对手的最佳反应策略
 """
 
 import numpy as np
 
-GameMatrix = np.array([
-    [-2, 3],
-    [3, -4]
-])
 
-index = 0
-p2_payoff = np.zeros((1, 2))
-p1_payoff = np.zeros((2, 1))
+def matrix_fictitious_play(matrix):
+    """
+    每个智能体拥有两个策略集: 最优策略集, 历史平均策略集.
+    Step 1: 依据对手历史平均策略集, 找到一个最优的针对策略。
+    Step 2: 根据历史平均策略和本轮最优策略更新自己的历史平均策略。
+    """
+    dim = matrix.shape[0]
+    row_player_strategy = np.random.uniform(0, 1, (dim, 1))
+    col_player_strategy = np.random.uniform(0, 1, (1, dim))
+    for i in range(10000):
+        # for row player
+        average = np.average(col_player_strategy, axis=0)  # Step 1: 获取对手的历史平均策略.
+        best_respond = np.zeros((dim, 1))
+        best_respond[np.argmax(matrix @ average.T)] = 1  # Step 2: 基于对手的历史平均策得到最优响应.
+        row_player_strategy = np.hstack((row_player_strategy, best_respond))
+        # for col player
+        average = np.average(row_player_strategy, axis=1)
+        best_respond = np.zeros((1, dim))
+        best_respond[:, np.argmin(average.T @ matrix)] = 1
+        col_player_strategy = np.vstack((col_player_strategy, best_respond))
+        if i % 10 == 0:
+            print("row", np.average(row_player_strategy, axis=1))
+            print("col", np.average(col_player_strategy, axis=0))
 
-for i in range(100000):
-    p1_payoff += GameMatrix[:, index][:, np.newaxis]
-    index = np.argmax(p1_payoff)
-    upper_policy = p1_payoff[index, :] / (i + 1)
-
-    p2_payoff += GameMatrix[index, :][np.newaxis, :]
-    index = np.argmin(p2_payoff)
-    low_policy = p2_payoff[:, index] / (i + 1)
-
-print("lower", low_policy)
-print("upper", upper_policy)
-
-
-# case 2
-
-# GameMatrix = np.array([[2,1], [1,1]])
-GameMatrix = np.array([[0,2,-1], [-1,0,1], [1,-1,0]])
-
-Itr = 10000
-
-
-def fp(GameMatrix):
-    """ Fictitious play on normal-from game."""
-    # a random initial step on row
-    row_value = np.zeros(GameMatrix.shape[0])
-    col_value = np.zeros(GameMatrix.shape[1])
-    min_id_list, max_id_list = [], []
-
-    for i in range(Itr):
-        # current l is row
-        max_id = np.argmax(row_value)  # row player is maximizer
-        max_id_list.append(max_id)
-        l = GameMatrix[max_id]  # l is column now
-        col_value += np.array(l)
-
-        # current l is column
-        min_id = np.argmin(col_value)  # column player is minimizer
-        min_id_list.append(min_id)
-        l = GameMatrix[:, min_id]  # l is row now
-        row_value += np.array(l)
-
-    # The statistical frequencies of occurrence of different entries are just their probability masses in final policy,
-    # which is the average best response over history.
-    # hist, _ = np.histogram(max_id_list, bins=GameMatrix.shape[0])  # np.histogram is the wrong function to use!
-    hist = np.bincount(max_id_list, minlength=GameMatrix.shape[0])
-    max_policy = hist / np.sum(hist)
-    hist = np.bincount(min_id_list, minlength=GameMatrix.shape[1])
-    min_policy = hist / np.sum(hist)
-
-    row_value = row_value / (i + 1)  # historical average, row value is lower bound
-    col_value = col_value / (i + 1)  # historical average, column value is upper bound
-    print(f'For row player, strategy is {max_policy}, game value (lower bound): {row_value}')
-    print(f'For column player, strategy is {min_policy}, game value (upper bound): {col_value}')
-
-    return (max_policy, min_policy), (row_value, col_value)
-
-
-fp(GameMatrix)
 
 if __name__ == "__main__":
-    pass
+    matrix_instance = np.array([
+        [0, -1, 1],
+        [1, 0, -1],
+        [-1, 1, 0]
+    ])
+    matrix_fictitious_play(matrix_instance)
+
