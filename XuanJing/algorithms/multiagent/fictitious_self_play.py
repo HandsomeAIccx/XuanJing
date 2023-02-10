4ea3718c919381a469a4e238a5a5a80bff04a779
hzq <tinyzqh@163.com>
2023-02-11 00:29:17 +0800

    [dev] modify fictitious self play

--- XuanJing/algorithms/multiagent/fictitious_self_play.py
+++ XuanJing/algorithms/multiagent/fictitious_self_play.py
@@ -1,72 +1,56 @@
-# -*- coding: utf-8 -*-
-# @Time    : 2022/9/27 8:56 下午
-# @Author  : Zhiqiang He
-# @Email   : tinyzqh@163.com
-# @File    : fictitious_self_play.py
-# @Software: PyCharm
-
 import numpy as np
 
-GameMatrix = np.array([[0, 2, -1], [-1, 0, 1], [1, -1, 0]])
-
 eta = 0.2
-avg_warmup = 0.2
-N = 300  # 采样多次获得平均策略
 
-row_value = np.zeros(GameMatrix.shape[0])
-col_value = np.zeros(GameMatrix.shape[1])
-
-min_id_list, max_id_list = [], []
-
-max_policy = 1./GameMatrix.shape[0] * np.ones(GameMatrix.shape[0])
-min_policy = 1./GameMatrix.shape[1] * np.ones(GameMatrix.shape[1])
 
 def sample_from_categorical(dist):
-    """
-    sample once from a categorical distribution, return the entry index.
-    dist: should be a list or array of probabilities for a categorical distribution
-    """
+    dist = [i / sum(dist) for i in dist]
     return np.argmax(np.random.multinomial(1, dist))
 
 
-for i in range(1000):
-    if i < avg_warmup or np.isnan(max_policy).any() or sample_from_categorical([1-eta, eta]):
-        max_id = np.argmax(row_value)
-        max_id_list.append(max_id)
-    col_value = np.zeros(GameMatrix.shape[1])
-    for j in range(N):
-        max_id = sample_from_categorical(max_policy)
-        l = GameMatrix[max_id]
-        col_value += np.array(l)
-    col_value = col_value / N
+def matrix_fictitious_self_play(matrix):
+    """
+    Best Respond的求解并不容易, 因此需要放宽Best Respond的求解精度.
+    1. Origin FSP:
+    Supervise Learning for achieving the average policy.  # 基于自己的历史信息获取 average policy.
+    Reinforcement Learning for reaching the best respond policy. 基于对手的历史信息, 获取Best Respond.
+    2. Non-Neural Version FSP:
+    """
+    dim = matrix.shape[0]
+    row_player_strategy = np.random.uniform(0, 1, (dim, 1))
+    col_player_strategy = np.random.uniform(0, 1, (1, dim))
+    for i in range(10000):
+        # for row player
+        average = np.average(col_player_strategy, axis=0)  # Step 1: 获取对手的历史平均策略.
+        best_respond = np.zeros((dim, 1))
+        if sample_from_categorical([1 - eta, eta]):  # Step 2.1: 基于对手的历史平均策得到最优响应.
+            best_respond[np.argmax(matrix @ average.T)] = 1
+        else:  # Step 2.2: 采样得到
+            idx = sample_from_categorical(np.average(col_player_strategy, axis=0))
+            best_respond[np.argmax(matrix[:, idx])] = 1
+        row_player_strategy = np.hstack((row_player_strategy, best_respond))
+        # for col player
+        average = np.average(row_player_strategy, axis=1)
+        best_respond = np.zeros((1, dim))
+        if sample_from_categorical([1 - eta, eta]):  # Step 2.1: 基于对手的历史平均策得到最优响应.
+            best_respond[:, np.argmin(average.T @ matrix)] = 1
+        else:  # Step 2.2: 采样得到
+            idx = sample_from_categorical(np.average(row_player_strategy, axis=1))
+            best_respond[:, np.argmax(matrix[idx, :])] = 1
 
-    if i < avg_warmup or np.isnan(min_policy).any() or sample_from_categorical([1-eta, eta]):
-        min_id = np.argmin(col_value)
-        min_id_list.append(min_id)
-    row_value = np.zeros(GameMatrix.shape[0])
-    for j in range(N):
-        min_id = sample_from_categorical(min_policy)
-        l = GameMatrix[:, min_id]
-        row_value += np.array(l)
-    row_value = row_value / N
-
-    # average policy for each player
-    hist = np.bincount(max_id_list, minlength=GameMatrix.shape[0])
-    max_policy = hist / np.sum(hist)
-    hist = np.bincount(min_id_list, minlength=GameMatrix.shape[1])
-    min_policy = hist / np.sum(hist)
-
-    row_value = row_value / (i+1)
-    col_value = col_value / (i+1)
-
-print("max policy", max_policy)
-print("min policy", min_policy)
-
-print("row value", row_value)
-print("col value", col_value)
-
+        col_player_strategy = np.vstack((col_player_strategy, best_respond))
+        if i % 10 == 0:
+            print("row", np.average(row_player_strategy, axis=1))
+            print("col", np.average(col_player_strategy, axis=0))
 
 
+if __name__ == "__main__":
+    matrix_instance = np.array([
+        [0, -1, 1],
+        [1, 0, -1],
+        [-1, 1, 0]
+    ])
+    matrix_fictitious_self_play(matrix_instance)
 
 
 
